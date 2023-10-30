8b9696111217f0376b13cc2049de6a8cf72c2581
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-10-30 13:50:35 +0900

    feat : searchBar feat add
    검색바 눌렀을때 UI 변경 요소 적용

--- withSooHyeon/Views/CoreViews/CommunityView.swift
+++ withSooHyeon/Views/CoreViews/CommunityView.swift
@@ -13,6 +13,7 @@ struct CommunityView: View {
     @State private var isSearching: Bool = false
     @State private var showingPopup = false
     @State private var selectedOption: String = "최신순"
+    @State private var isTappedSearchBar: Bool = false
     
     func toastView() -> some View{
         VStack(alignment: .leading){
@@ -44,102 +45,144 @@ struct CommunityView: View {
         
     }
     
+    func performSearch(){
+        
+    }
+    
+    func hideKeyboard() {
+           // 키보드를 닫는 함수
+           UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+       }
+    
     var body: some View {
         ZStack(){
             VStack (spacing: 0){
+                
                 //첫번째 Stack
                 HStack{
-                    Rectangle()
-                        .frame(height: 40)
-                        .cornerRadius(40)
-                        .foregroundColor(Color(hex: 0xF6FBFF))
-                        .border(Color(hex: 0xCCE1FF),width: 1)
-                        .overlay(
-                                                HStack {
-                                                    Image(systemName: "magnifyingglass")
-                                                    TextField("검색", text: $searchText)
-                                                    if isSearching {
-                                                        Button(action: {
-                                                            searchText = ""
-                                                            isSearching = false
-                                                            // 여기에서 검색 기능 실행
-                                                        }) {
-                                                            Image(systemName: "xmark.circle.fill")
-                                                        }
-                                                    }
-                                                }
-                                                )
-                        .onTapGesture {
-                                                isSearching = true
-                                            }
+                    
+                    HStack{
+                        if isTappedSearchBar{
+                            Image(systemName: "chevron.left")
+                                .font(.system(size: 23,weight: .bold))
+                                .foregroundColor(Color(hex: 0x2D3740))
+                                .onTapGesture {
+                                    isTappedSearchBar=false
+                                }
+                        }else{
+                            Image(systemName: "magnifyingglass")
+                                .font(.system(size: 23,weight: .bold))
+                                .foregroundColor(Color(hex: 0x2D3740))
+                        }
+                        
+                        TextField("글 제목, 내용, 해시태그, 작성자 아이디",text: $searchText, onCommit:{
+                            performSearch()
+                        })
+                            .foregroundColor(.black)
+                            .onTapGesture {
+                                isTappedSearchBar = true
+                            }
+                        if isTappedSearchBar{
+                            Image(systemName: "xmark")
+                                .font(.system(size: 20,weight: .bold))
+                                .foregroundColor(Color(hex: 0x2D3740))
+                                .padding(.trailing,8)
+                                .onTapGesture {
+                                    searchText = ""
+                                }
+                        }
+                    }
+                    .padding(.top,8)
+                    .padding(.bottom,8)
+                    .padding(.leading,12)
+                    .foregroundColor(Color(hex: 0xF6FBFF))
+                    .frame(height: 40)
+                    .background(Color(hex: 0xF6FBFF))
+                    .cornerRadius(40)
+//                    .overlay(
+//                           Rectangle()
+//                               .stroke(Color(hex: 0xCCE1FF), lineWidth: 1)
+//                       )
                     Image(systemName: "bell.fill")
                         .font(.system(size: 28))
-                }
-                if isSearching {
-                                Text("검색 결과: \(searchText)")
-                            }
-                
-                //두번째 Stack
-                HStack{
-                    VStack(alignment: .leading){
-                        Text("전국의")
-                            .font(.system(size: 22))
-                            .fontWeight(.black)
-                            .padding(.bottom,0.1)
-                        Text("수현이와 나누는")
-                            .font(.system(size: 22))
-                            .fontWeight(.black)
-                            .padding(.bottom,0.1)
-                        Text("고민상담소")
-                            .font(.system(size: 22))
-                            .fontWeight(.black)
-                            .padding(.bottom,0.1)
-                        Text("수현이에게 고민을 이야기 해봐요!")
-                            .font(.system(size: 12))
-                    }
-                    Spacer()
-                    Rectangle()
-                        .frame(width: 130, height: 130)
-                        .foregroundColor(Color(hex: 0xECF5FE))
-                }
+                }//첫번째 Stack end
                 .padding(.leading,27)
                 .padding(.trailing,27)
-                .padding(.top,23)
                 .padding(.bottom,23)
-                
-                //세번째 Stack
-                VStack{
-                    HStack{
-                        Text("수현이들 고민거리")
-                            .font(.system(size: 16))
-                            .fontWeight(.black)
-                        Spacer()
-                        Button(action: {
-                            self.showingPopup = true
-                                }) {
-                                    HStack {
-                                        Text("\(selectedOption)")
-                                            .font(.system(size: 10))
-                                            .foregroundColor(Color(hex: 0x90ADC7))
-                                        Image(systemName: "chevron.down")
-                                            .font(.system(size: 10))
-                                            .foregroundColor(Color(hex: 0x90ADC7))
-                                    }
-                                }
-                    }
-                    ScrollView{
-                        CommunityCardView()
-                        CommunityCardView()
-                        CommunityCardView()
-                        CommunityCardView()
-                    }
+                if isTappedSearchBar{
+                    Spacer().frame(height: 0)
                 }
-                .padding(.leading,27)
-                .padding(.trailing,27)
-                .padding(.top,23)
-                .background(Color(hex: 0xECF5FE))
+                ScrollView{
+                    //두번째 Stack
+                    if !isTappedSearchBar{
+                        HStack{
+                            VStack(alignment: .leading){
+                                Text("전국의")
+                                    .font(.system(size: 22))
+                                    .fontWeight(.black)
+                                    .padding(.bottom,0.1)
+                                Text("수현이와 나누는")
+                                    .font(.system(size: 22))
+                                    .fontWeight(.black)
+                                    .padding(.bottom,0.1)
+                                Text("고민상담소")
+                                    .font(.system(size: 22))
+                                    .fontWeight(.black)
+                                    .padding(.bottom,0.1)
+                                Text("수현이에게 고민을 이야기 해봐요!")
+                                    .font(.system(size: 12))
+                            }
+                            Spacer()
+                            Rectangle()
+                                .frame(width: 130, height: 130)
+                                .foregroundColor(Color(hex: 0xECF5FE))
+                        }//두번째 Stack end
+                        .padding(.leading,27)
+                        .padding(.trailing,27)
+                        .padding(.bottom,23)
+                        .background(Color.white)
+                    }
+                    //세번째 Stack
+                    VStack{
+                        if !isTappedSearchBar{
+                            HStack{
+                                Text("수현이들 고민거리")
+                                    .font(.system(size: 16))
+                                    .fontWeight(.black)
+                                Spacer()
+                                Button(action: {
+                                    self.showingPopup = true
+                                        }) {
+                                            HStack {
+                                                Text("\(selectedOption)")
+                                                    .font(.system(size: 10))
+                                                    .foregroundColor(Color(hex: 0x90ADC7))
+                                                Image(systemName: "chevron.down")
+                                                    .font(.system(size: 10))
+                                                    .foregroundColor(Color(hex: 0x90ADC7))
+                                            }
+                                        }
+                            }
+                        }
+                        CommunityCardView()
+                        CommunityCardView()
+                        CommunityCardView()
+                        CommunityCardView()
+                        CommunityCardView()
+                        CommunityCardView()
+                        CommunityCardView()
+                    } //세번째 Stack end
+                    .padding(.leading,27)
+                    .padding(.trailing,27)
+                    .padding(.top,23)
+                }.background(Color(hex: 0xECF5FE))
+                
+               
                 
             }//VStack
+            .onTapGesture {
+                hideKeyboard()
+            }
             
         }//ZStack
         .popup(isPresented: $showingPopup){
